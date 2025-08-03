2a4d1feacec16f4cd38430b2bbd967d591e338e6
Udeme Dunstan Inyang <udemedunstan@gmail.com>
2025-08-03 20:37:29 +0100

    commit with some error

--- app/src/main/java/com/example/translation/MainActivity.kt
+++ app/src/main/java/com/example/translation/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
@@ -20,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
        val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val translateButton = findViewById<Button>(R.id.TranslateButton)
         val outputText = findViewById<TextView>(R.id.outputText)
